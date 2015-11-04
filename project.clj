9ff5d626832fb04a094d4c7c29d00775c1df7513
James Prokash <jprokash@carelogistics.com>
2015-11-04 11:04:51 -0500

    Emit Exception logging in Spy notification

--- project.clj
+++ project.clj
@@ -13,4 +13,5 @@
                   :scope "provided"
                   :exclusions [joda-time]]
                  [clj-time "0.11.0"
-                  :scope "provided"]])
+                  :scope "provided"]
+                 [org.clojure/tools.logging "0.3.1"]])
